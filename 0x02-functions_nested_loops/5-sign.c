7d42d15ca21a151ef9da1186c3339ffc85d66aa5
niffy23 <niffyfunmi16@gmail.com>
2022-12-13 20:12:35 -0800

    commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_sign-prints thesignof anumber
+ * @n:the number of the signtobe printed
+ * Return: 1 if number is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('_');
+		return (-1);
+	}
+}
