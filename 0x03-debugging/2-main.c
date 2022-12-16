57dce18e6cbbb8b8d6fa69c5d354b4eb22655efa
nifemi <niffyfunmi16@gmail.com>
2022-12-16 22:55:10 +0100

    Update 2-main.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -4,10 +4,9 @@
 /**
 * main - prints the largest of 3 integers
 * Return: 0
-
-
-* int main(void)
 */
+
+int main(void)
 {
         int a, b, c;
         int largest;
@@ -17,7 +16,7 @@
         c = 0;
 
         largest = largest_number(a, b, c);
-        
+
         printf("%d is the largest number\n", largest);
 
         return (0);
