4817aa5d2daca602a61677c25cbebb02d417f5ab
niffy23 <niffyfunmi16@gmail.com>
2022-12-15 18:41:05 -0800

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,22 @@
  * Return: value of the last digit
  */
 int print_last_digit(int n)
+
 {
-	int ld = n % 10;
 
-	if (ld < 0)
-		ld *= -1;
+	int last_digit;
 
-	_putchar(ld + '0');
-
-	return (0);
+	if (n < 0)
+	{
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
 }
+
