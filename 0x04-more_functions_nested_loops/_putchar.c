311a3a37a5a77eeaa6c47a4e101af4943deae89d
niffy23 <niffyfunmi16@gmail.com>
2022-12-15 20:53:37 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to tdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 returned, and error is not set appropriately                                                                      		
+-
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
