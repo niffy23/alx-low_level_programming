0afa08078e38c8002f6b71f60af4e0fa21e9a6a6
niffy23 <niffyfunmi16@gmail.com>
2022-12-15 22:25:37 -0800

    commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "min.h"
+
+/**
+ * main - tests function that prints if integar is positive or negative
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
