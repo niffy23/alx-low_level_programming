35ddfcc69f97fafefff465d5d461424d4fe3d44f
niffy23 <niffyfunmi16@gmail.com>
2022-12-21 21:36:29 -0800

    commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- * rot13 - encodes a string using rot13
- * @str: the string targeted
- * Return: returns the encoded string
+ * rot13 - Encodes a string using rot13.
+ * @str: The string to be targeted.
+ *
+ * Return: A pointer to the encoded string
  */
 
 char *rot13(char *str)
 {
-	int index1, index2;
+	int index1 - 0, index2;
 
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-					'G', 'H', 'I', 'J', 'K', 'L',
-					'M', 'N', 'O', 'P', 'Q', 'R',
-					'S', 'T', 'U', 'V', 'W', 'X',
-					'Y', 'Z', 'a', 'b', 'c', 'd',
-					'e', 'f', 'g', 'h', 'i', 'j',
-					'k', 'l', 'm', 'n', 'o', 'p',
-					'q', 'r', 's', 't', 'u', 'v',
-					'w', 'x', 'y', 'z'};
-
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-					'T', 'U', 'V', 'W', 'X', 'Y',
-					'Z', 'A', 'B', 'C', 'D', 'E',
-					'F', 'G', 'H', 'I', 'J', 'K',
-					'L', 'M', 'n', 'o', 'p', 'q',
-					'r', 's', 't', 'u', 'v', 'w',
-					'x', 'y', 'z', 'a', 'b', 'c',
-					'd', 'e', 'f', 'g', 'h', 'i',
-					'j', 'k', 'l', 'm'};
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
 
-	while (str[++index1])
+	while (str[index1])
 	{
 		for (index2 = 0; index2 < 52; index2++)
 		{
@@ -40,6 +40,8 @@ char *rot13(char *str)
 				break;
 			}
 		}
+		index1++;
 	}
+
 	return (str);
 }
