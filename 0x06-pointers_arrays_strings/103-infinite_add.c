7fde13cc4a5ce58d7477c6bc21fb05b8925a5639
nifemi <niffyfunmi16@gmail.com>
2022-12-22 14:09:20 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -49,7 +49,8 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		return (0);
 
 	return (r + r_index + 1);
-}/**
+}
+/**
  * infinite_add - Adds two numbers.
  * @n1: The first number to be added.
  * @n2: The second number to be added.
@@ -76,5 +77,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	n2 += n2_len - 1;
 	*(r + size_r) = '\0';
 
-	return (add_strings(n1, n2, r, size_r));
+	return (add_strings(n1, n2, r, --size_r));
 }
