9ad7a92fc03eba1eaf72fbeb85cb533fb3eb51e1
nifemi <niffyfunmi16@gmail.com>
2022-12-22 05:13:23 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,8 @@
 #include "main.h"
-
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to capitalized.
- *
- * Return: A pointer to the changed string.
+ * cap_string - function that capitalize first character of a word
+ * @str: string to capitalize
+ * Return: returns the capitalized string
  */
 char *cap_string(char *str)
 {
@@ -27,11 +25,8 @@ char *cap_string(char *str)
 				str[index - 1] == '(' ||
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
-				index == 0)
-				  str[index] -= 32;
-				  
-			index++;
+				str[index - 1] == '}')
+			str[index] -= 32;
 	}
 	return (str);
 }
