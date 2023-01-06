33b671cb2a8383b25ffb1005615c02afa115c8f5
nifemi <niffyfunmi16@gmail.com>
2023-01-06 04:49:07 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,9 +2,9 @@
 
 /**
  * _memset - Fill memory with a constant byte
- * @s: memory area to fill.
- * @b: constant byte to fill.
- * @n: bytes of memory to fill.
+ * @s: memory area to fill
+ * @b: constant byte to fill
+ * @n: bytes of memory area to fill
  *
  * Return: the memory area filled.
  */
