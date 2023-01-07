39e013aab4748e0ad41235c42f81dc06c581f165
nifemi <niffyfunmi16@gmail.com>
2023-01-07 20:40:53 +0100

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main(_putchar)int
+	
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	_puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 
