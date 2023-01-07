eb9e023944278668aa1c0cd2e6d2e2a092a65b49
nifemi <niffyfunmi16@gmail.com>
2023-01-07 17:53:54 +0100

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,14 @@
-#include "main.h"
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
- * void _puts(char *str)
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
+
 }
